34ba63a8127e306df50c45c4cf65e49fa1b4dea6
OlyaBorisevich <borisevichilya94@gmail.com>
2020-07-22 15:51:38 +0300

    Новая версия

---
+++ schoolLA/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        dots: true,
+        nav: true,
+        navText: ['', '']
+    });    
+});
+
+/*<i class="fas fa-chevron-left"></i>
+<i class="fas fa-chevron-right"></i>*/
