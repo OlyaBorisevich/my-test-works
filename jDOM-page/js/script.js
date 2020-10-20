c247d400d7068ec38f6d627196617acdc0b570c0
OlyaBorisevich <borisevichilya94@gmail.com>
2020-10-20 08:19:43 +0300

    fix

--- jDOM-page/js/script.js
+++ jDOM-page/js/script.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-  $(".slider-block").owlCarousel({
-      items: '2',
-      dots: false
-  });
+$(document).ready(function () {
+    $(".slider-block").owlCarousel({
+        items: '2',
+        dots: false,
+        nav: true,
+        navText: [
+          `<svg class="prev" xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20">
+            <use xlink:href="#arrow"></use>
+          </svg>`,
+          `<svg class="next" xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20">
+            <use xlink:href="#arrow"></use>
+          </svg>`
+      ]
+    });
 });
