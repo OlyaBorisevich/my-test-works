e0b0ec70ebf60492329229de66f4d06dfa4c6e8d
Olya <borisevicholya94@gmail.com>
2020-10-20 18:56:03 +0300

    fix

--- jDOM-page/js/script.js
+++ jDOM-page/js/script.js
@@ -7,7 +7,11 @@ $(document).ready(function () {
           `<svg class="prev" xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20">
             <use xlink:href="#arrow"></use>
           </svg>`,
-          `<svg class="next" xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20">
+          `
+          <svg class="bg-arrow xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="73px" height="70px">
+            <use xlink:href="#bg-arrow"></use>
+          </svg>
+          <svg class="next" xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20">
             <use xlink:href="#arrow"></use>
           </svg>`
       ]
